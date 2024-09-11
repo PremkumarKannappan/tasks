b0fa3f46fc51ec21b824fbf36bc0e4ee121f2094
PremkumarKannappan <premkumar.kannappan@finsurge.tech>
2024-09-11 07:59:18 +0530

    task

--- Micro/eurekaserver (1)/src/main/java/com/main/eurekaserver/EurekaServerApplication.java
+++ Micro/eurekaserver (1)/src/main/java/com/main/eurekaserver/EurekaServerApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaserverApplication {
+@EnableEurekaServer    //Annotation to set that this is a eureka server and need to add dependency eureka-server in pom
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
